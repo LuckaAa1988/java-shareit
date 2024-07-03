43c2f20fd17331b7440ccd298375a8acfd13f811
Luckaaa <perepelitsalesha@yandex.ru>
2024-07-04 00:00:37 +0300

    delete unused imports

--- gateway/src/main/java/ru/practicum/shareit/user/client/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/user/client/UserClient.java
@@ -1,13 +1,8 @@
 package ru.practicum.shareit.user.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
 import ru.practicum.shareit.client.RestTemplateFactory;
 import ru.practicum.shareit.user.dto.UserRequest;
